cccf44593061a50433ae7679de639db478d4f682
Salman Shahid <salmanshahid0333@gmail.com>
2025-04-28 11:07:20 -0700

    user model and driver model create

---
+++ models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 characters long'],
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must be at least 3 characters long'],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [4, 'Email must be at least 4 characters long'],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String,
+    },
+}, { timestamps: true })
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    return token;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+
+export default mongoose.model("User", userSchema);
