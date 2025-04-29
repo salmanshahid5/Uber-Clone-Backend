92e0e3d27f2ef330a67490a2f98bc71871b3ff5f
Salman Shahid <salmanshahid0333@gmail.com>
2025-04-29 04:35:55 -0700

    user controller create

---
+++ controllers/user.controller.js
@@ -0,0 +1,78 @@
+import userModel from "../models/user.model.js"
+import AddUser from "../services/user.service.js"
+import { validationResult } from "express-validator"
+import blacklistTokenModel from "../models/blacklistToken.model.js"
+
+// register user
+export const registerUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { fullname, email, password } = req.body;
+
+    const isUserAlready = await userModel.findOne({ email });
+
+    if (isUserAlready) {
+        return res.status(400).json({ message: 'User already exist' });
+    }
+
+    const hashedPassword = await userModel.hashPassword(password);
+
+    const user = await AddUser({
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
+        email,
+        password: hashedPassword
+    });
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ token, user });
+}
+
+// login user
+export const loginUser = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, user });
+}
+
+// user profile
+export const getUserProfile = async (req, res, next) => {
+    res.status(200).json(req.user);
+}
+
+//logout
+export const logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blacklistTokenModel.create({ token });
+
+    res.status(200).json({ message: 'Logged out' });
+
+}
